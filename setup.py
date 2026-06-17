bd56158a5c166d2f445c17a62a1eb69c24cac8c0
aman <aman@amans-MacBook-Air.local>
2026-06-17 13:07:27 +0530

    chore: rename distribution package to mockmesh-aws and update repository URLs

--- setup.py
+++ setup.py
@@ -3,17 +3,16 @@ import os
 from setuptools import setup, find_packages
 
 setup(
-    name="mockmesh",
+    name="mockmesh-aws",
     version="0.2.0",
     author="Aman Kumar",
     description="An in-process, schema-driven, zero-dependency AWS auto-mocking engine for Python developers",
     long_description=open("README.md").read() if os.path.exists("README.md") else "",
     long_description_content_type="text/markdown",
-    url="https://github.com/amankumar/mockmesh",
+    url="https://github.com/devil15371/mockmesh",
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         "Intended Audience :: Developers",
         "Topic :: Software Development :: Testing",
